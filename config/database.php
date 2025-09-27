052e2d810e8f225dc49109ec40b681b2e202410b
Ivan <tylerfernandez140@gmail.com>
2025-09-27 13:14:45 +0800

    Fix: Configure PostgreSQL connection for Neon.tech

--- config/database.php
+++ config/database.php
@@ -94,7 +94,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'require'),
+            'options' => array_filter([
+                'endpoint' => env('DB_OPTIONS'),
+            ]),
         ],
 
         'sqlsrv' => [
